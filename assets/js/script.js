2fbd518e8f65c9fdb01baba8eaedc73c285c18a3
Nojyto <nojusskirkevicius@gmail.com>
2024-01-20 14:11:53 +0200

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+function copyEmailToClipboard() {
+    navigator.clipboard.writeText("nojusskirkevicius@gmail.com");
+    alert("Copied email link to clipboard: nojusskirkevicius@gmail.com");
+}
