51fecdf3984bf41467e87de19cfa7ae950c7a13e
Nojyto <nojusskirkevicius@gmail.com>
2024-02-03 20:33:39 +0200

    4.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-VANTA.NET({
+const vantaBasicSettings = {
     el: "#animated-background",
     mouseControls: true,
     touchControls: true,
@@ -10,16 +9,60 @@ VANTA.NET({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
+    color: 0xec2727,
+    backgroundColor: 0x0d0d0d
+};
+
+const vantaMobileSettings = {
+    points: 12.00,
+    maxDistance: 20.00,
+    spacing: 18.00
+};
+
+const vantaDesktopSettings = {
     points: 20.00,
     maxDistance: 24.00,
     spacing: 18.00,
-    color: 0xec2727,
-    backgroundColor: 0x0d0d0d
-})
+}
 
+
+AOS.init();
+if (window.innerWidth < 760) {
+    VANTA.NET({ ...vantaBasicSettings, ...vantaMobileSettings });
+} else {
+    VANTA.NET({ ...vantaBasicSettings, ...vantaDesktopSettings })
+}
+
+
+const SendToTopButton = document.getElementById("toTopBtn");
+const ScrollIndicator = document.getElementById("scrollIndicator");
+
+
+window.onload = async () => {
+    await delay(5000);
+    ScrollIndicator.style.display = "block";
+}
+
+window.onscroll = () => {
+    scrollFunction();
+    ScrollIndicator.style.display = "none";
+};
 
 window.addEventListener("contextmenu", e => e.preventDefault());
 
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        SendToTopButton.style.display = "block";
+    } else {
+        SendToTopButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
